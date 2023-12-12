1dabe7ab5d2d622da5f44e4a88d968c08834e0ce
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-12 02:04:55 +0200

    uploaded lab4

---
+++ lab4/token_auth/index.js
@@ -0,0 +1,148 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+const port = 3000;
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+class AuthService {
+  async login(username, password) {
+    const options = {
+      method: 'POST',
+      url: `${process.env.DOMAIN}/oauth/token`,
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      form: {
+        grant_type: 'http://auth0.com/oauth/grant-type/password-realm',
+        realm: process.env.CONNECTION,
+        scope: 'offline_access',
+        username: username,
+        password: password,
+        audience: process.env.AUDIENCE,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+      },
+    };
+    try {
+      const response = await fetch(options.url, {
+        method: options.method,
+        headers: options.headers,
+        body: new URLSearchParams(options.form),
+      });
+      return await response.json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async register(login, password) {
+    const data = {
+      email: login,
+      connection: process.env.CONNECTION,
+      password,
+    };
+    const headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${process.env.ADMIN_TOKEN}`,
+    };
+    const url = process.env.AUDIENCE + '/users';
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data),
+      });
+      return await response.json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async refresh(refreshToken) {
+    const options = {
+      method: 'POST',
+      url: `${process.env.DOMAIN}/oauth/token`,
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      form: {
+        grant_type: 'refresh_token',
+        scope: 'offline_access',
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        refresh_token: refreshToken,
+      },
+    };
+    try {
+      const response = await fetch(options.url, {
+        method: options.method,
+        headers: options.headers,
+        body: new URLSearchParams(options.form),
+      });
+      return await response.json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+app.get('/', (req, res) => {
+  if (req.session && req.session.username) {
+    return res.json({
+      username: req.session.username,
+      logout: 'http://localhost:3000/logout',
+    });
+  }
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/logout', (req, res) => {
+  res.redirect('/');
+});
+
+app.post('/api/login', async (req, res) => {
+  const { login, password } = req.body;
+  try {
+    const { access_token, expires_in, refresh_token } = await authService.login(
+      login,
+      password
+    );
+    res.send({ token: access_token, expires_in, refresh_token });
+  } catch (e) {
+    console.log(e);
+    res.status(401).send();
+  }
+});
+
+app.post('/api/refresh', async (req, res) => {
+  const { refreshToken } = req.body;
+  try {
+    const { access_token, expires_in, refresh_token } =
+      await authService.refresh(refreshToken);
+    res.send({ token: access_token, expires_in, refresh_token });
+  } catch (e) {
+    console.log(e);
+    res.status(401).send();
+  }
+});
+
+app.post('/api/register', async (req, res) => {
+  const { login, password } = req.body;
+  try {
+    const { access_token, expires_in } = await authService.register(
+      login,
+      password
+    );
+    res.send({ token: access_token, expires_in });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ message: 'Error', error: e });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
